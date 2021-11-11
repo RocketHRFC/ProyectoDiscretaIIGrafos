d420ec639da9624e8a5faaf394c56e946545a11f
SAMER ARANKI MUNOZ <saranki@correo.url.edu.gt>
2021-11-11 16:52:41 -0600

    Push de prueba

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace ProyectoDiscretaIIGrafos
     internal class Program
     {
         //Cambio Prueba Hugo Florian
-
+        //Cambio Prueba Samer Aranki
         static void Main(string[] args)
         {
         }
